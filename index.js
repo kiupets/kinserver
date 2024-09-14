29ebfb0a334af3dfb4b49dfc8d9233a8855114a8
kiupets <gonzalogg1975@gmail.com>
2024-09-14 01:58:17 -0300

    v1

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24, // 1 day
     // httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
     sameSite: 'lax',
   },
   resave: false,
