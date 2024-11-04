3a8e420abad69f71129ae408cc5bc63e68123519
kiupets <gonzalogg1975@gmail.com>
2024-11-04 13:17:27 -0300

    fuck

--- index.js
+++ index.js
@@ -77,6 +77,8 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     secure: true,
+    sameSite: 'none',
+    httpOnly: true
   },
   store: store,
   resave: false,
